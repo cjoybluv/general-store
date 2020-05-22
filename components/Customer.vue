73b351a8e7f94ec65dde2899b6a244f207012be7
Dave Hemmer <dave_hemmer@hotmail.com>
2020-05-21 18:05:20 -0700

    init wiring

---
+++ components/Customer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ customer.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    customer: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/customers.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Customer :customer="customer" />
+  </div>
+</template>
+
+<script>
+import Customer from '@/components/Customer.vue'
+export default {
+  name: 'Customers',
+  components: {
+    Customer
+  },
+  data() {
+    return {
+      customers: [
+        {
+          name: 'Dave',
+          street: '123 Here',
+          city: 'Now',
+          state: 'OR',
+          zip: '97123'
+        }
+      ]
+    }
+  },
+  computed: {
+    customer() {
+      return this.customers[0]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
